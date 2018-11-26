c616147cbca6f6301f90d14a63196caf86a9d3f0
2147582 <2147582@slu.edu.ph>
2018-11-26 13:18:46 +0800

    Changes in the nav bar content

--- Client/js/main.js
+++
@@ -1,8 +0,0 @@
-var div = $('#login-modal');  // container for the content
-
-$.get('../login-modal.html')                      // get the content
- .success(function(resp) {      // on success
-   $(div)
-      .html(resp)               // populate the div
-      .dialog({ modal: true }); // and turn it into a dialog
- });

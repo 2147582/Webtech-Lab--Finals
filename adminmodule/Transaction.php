1cf6e60e15424b6eeab95473adf7e64db247122a
Hrellano <44044518+Hrellano@users.noreply.github.com>
2018-11-29 11:47:30 +0800

    added admin module

---
+++ adminmodule/Transaction.php
@@ -0,0 +1,238 @@
+<?php
+include 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Dashboard</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/theme.css" rel="stylesheet" media="all">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body class="animsition">
+    <div class="page-wrapper">
+
+        <!-- MENU SIDEBAR-->
+        <aside class="menu-sidebar d-none d-lg-block">
+
+        <!-- WEBLAB LOGO -->
+            <div class="logo">
+                <a href="index.php">
+                    <!-- <img src="images/icon/logo.png" alt="Cool Admin" /> -->
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-chart-bar"></i>Services</a>
+                                <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                 <li>
+                                    <a href="products.php">Post Equipments</a>
+                                </li>
+                                <li>
+                                    <a href="Add.php">View Equipments</a>
+                                </li>
+                            </ul>
+                        </li>               
+                        <li>
+                        <a class="js-arrow" href="#">                         
+                                <i class="fas fa-table"></i>Transactions</a>
+                                <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                 <li>
+                                    <a href="AcceptTransanction.php">Accepted Transactions</a>
+                                </li>
+                                <li>
+                                    <a href="Transaction.php">View Transactions</a>
+                                </li>
+                            </ul>
+                            </li>
+                            <li>
+                            <a class="js-arrow" href="#">                                
+                                    <i class="far fa-check-square"></i>User Management</a>
+                                   <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                 <li>
+                                    <a href="products.php">Registered Accounts</a>
+                                </li>
+                                <li>
+                                    <a href="Add.php">Disabled Accounts</a>
+                                </li>
+                            </ul>
+                                
+                                </ul>
+                            </nav>
+                        </div>
+                    </aside>
+                    <!-- END MENU SIDEBAR-->
+                    <!-- PAGE CONTAINER-->
+                    <div class="page-container">
+                        <!-- HEADER DESKTOP-->
+                        <header class="header-desktop">
+                            <div class="section__content section__content--p30" align="center">
+                                <iframe src="http://free.timeanddate.com/clock/i6cof2h4/n145/fn8/fs20/ftb/pa10/tt0/tm1/td1/th2/tb1" frameborder="0" width="453" height="43"></iframe>
+                            </div>
+                        </header>
+                        <!-- HEADER DESKTOP-->
+    <!-- MAIN CONTENT-->
+       <div class="main-content">
+      <div class="section__content section__content--p100">  
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 mx-auto">
+              <div class="card" style="box-shadow: 0 0 25px 0 lightgrey;">
+                <div class="card-header"><h4 align="center">Transactions</h4></div>
+                  <div class="card-body">
+  <table class="table table-borderless table-data3">
+                                        <thead>
+                                            <tr>
+                                                <th>Date Rented</th>
+                                                <th>Date Returned</th>
+                                                <th>type</th>
+                                                <th>description</th>
+                                                <th>status</th>
+                                                <th>price</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>2018-09-29 05:57</td>
+                                               
+                                                <td>2018-09-29 05:57</td>
+                                                <td>Mobile</td>
+                                                <td>iPhone X 64Gb Grey</td>
+                                                <td class="process">Processed</td>
+                                                <td>$999.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-28 01:22</td>
+                                                <td>2018-09-28 01:22</td>
+                                                
+                                                <td>Mobile</td>
+                                                <td>Samsung S8 Black</td>
+                                                <td class="process">Processed</td>
+                                                <td>$756.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-27 02:12</td>
+                                               
+                                                <td>2018-09-28 01:22</td>
+                                                <td>Game</td>
+                                                <td>Game Console Controller</td>
+                                                <td class="denied">Denied</td>
+                                                <td>$22.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-26 23:06</td>
+                                                
+                                                <td>2018-09-26 23:06</td>
+                                                <td>Mobile</td>
+                                                <td>iPhone X 256Gb Black</td>
+                                                <td class="denied">Denied</td>
+                                                <td>$1199.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-25 19:03</td>
+                                               
+                                                <td>2018-09-25 19:03</td>
+                                                <td>Accessories</td>
+                                                <td>USB 3.0 Cable</td>
+                                                <td class="process">Processed</td>
+                                                <td>$10.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-29 05:57</td>
+                                               
+                                                <td>2018-09-29 05:57</td>
+                                                <td>Accesories</td>
+                                                <td>Smartwatch 4.0 LTE Wifi</td>
+                                                <td class="denied">Denied</td>
+                                                <td>$199.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-24 19:10</td>
+                                                
+                                                <td>2018-09-24 19:10</td>
+                                                
+                                                <td>Camera</td>
+                                                <td>Camera C430W 4k</td>
+                                                <td class="process">Processed</td>
+                                                <td>$699.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-22 00:43</td>
+                                                
+                                                <td>2018-09-22 00:43</td>
+                                                
+                                                <td>Computer</td>
+                                                <td>Macbook Pro Retina 2017</td>
+                                                <td class="process">Processed</td>
+                                                <td>$10.00</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                  </div>
+    <!-- END MAIN CONTENT-->
+    <!-- END PAGE CONTAINER-->
+  </div>
+</div>
+
+<!-- Jquery JS-->
+<script src="vendor/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap JS-->
+<script src="vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<!-- Vendor JS       -->
+<script src="vendor/slick/slick.min.js">
+</script>
+<script src="vendor/wow/wow.min.js"></script>
+<script src="vendor/animsition/animsition.min.js"></script>
+<script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+</script>
+<script src="vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="vendor/counter-up/jquery.counterup.min.js">
+</script>
+<script src="vendor/circle-progress/circle-progress.min.js"></script>
+<script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="vendor/select2/select2.min.js">
+</script>
+
+<!-- Main JS-->
+<script src="js/main.js"></script>
+
+</body>
+</html>
+<!-- end document-->
